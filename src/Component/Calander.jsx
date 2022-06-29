77d11939eaae0fa068fc80c1d0702b328b479c52
ash12-coder <ashish.agrawal_cs18@gla.ac.in>
2022-06-29 13:15:59 +0530

    changes

--- src/Component/Calander.jsx
+++ src/Component/Calander.jsx
@@ -24,6 +24,245 @@ export default function Calander() {
   // console.log("cbejbce3be3knkrenf", +moment(selectDate).format("MM"));
 
   // console.log("months", months[currentMonth]);
+  // return (
+  //   // <>
+  //   // <div className="container-fluid">
+  //   //   <div className="header d-flex justify-content-between align-items-center p-3">
+  //   //     <div>
+  //   //       <span className="fw-bold mx-2" style={{ fontSize: "50px" }}>
+  //   //         {months[+moment(selectDate).format("MM") - 1]}
+  //   //       </span>{" "}
+  //   //       <span className="h1 fw-normal" style={{ fontSize: "50px" }}>
+  //   //         {moment(selectDate).format("YYYY")}
+  //   //       </span>
+  //   //     </div>
+  //   //     <div>
+  //   //       <span>
+  //   //         <button onClick={getPrevMonth} className="btn  border border-2">
+  //   //           <GrPrevious />
+  //   //         </button>
+  //   //         <button className="btn border border-2 mx-2" onClick={getToday}>
+  //   //           Today
+  //   //         </button>
+  //   //         <button onClick={getNextMonth} className="btn border border-2">
+  //   //           <GrNext />
+  //   //         </button>
+  //   //       </span>
+  //   //     </div>
+  //   //   </div>
+  //   //   <div className="h-100 w-100  text-end">
+  //   //     <div className="row">
+  //   //       {days.map((val) => {
+  //   //         return (
+  //   //           <>
+  //   //             <div className="col" key={val}>
+  //   //               <h3 className="fw-normal">{val}</h3>
+  //   //             </div>
+  //   //           </>
+  //   //         );
+  //   //       })}
+  //   //     </div>
+
+  //   // {Object.values(calanderRow).map((val) => {
+  //   //   return (
+  //   //     <div className="row" style={{ height: "135px" }}>
+  //   //       {val.map((value, i) => {
+  //   //         console.log("", +moment(selectDate).format("MM"));
+
+  //   //         return (
+  //   //           <>
+  //   //             <div
+  //   //               className="col border"
+  //   //               style={{
+  //   //                 background: `${i === 0 ? "#f0f0f2" : ""}`,
+  //   //               }}
+  //   //             >
+  //   //               {value.date === 1 ? (
+  //   //                 <>
+  //   //                   <span
+  //   //                     className={`${
+  //   //                       value.month === months[currentMonth] &&
+  //   //                       value.date === currentDate &&
+  //   //                       value.year === currentYear
+  //   //                         ? "text-success"
+  //   //                         : "bg-danger"
+  //   //                     }`}
+  //   //                   >
+  //   //                     <h2
+  //   //                       className={`${
+  //   //                         value.month !==
+  //   //                         months[+moment(selectDate).format("MM") - 1]
+  //   //                           ? "text-secondary"
+  //   //                           : ""
+  //   //                       } mt-3`}
+  //   //                     >
+  //   //                       {value.date} {value.month.slice(0, 3)}
+  //   //                     </h2>
+  //   //                   </span>
+  //   //                 </>
+  //   //               ) : (
+  //   //                 <>
+  //   //                   <span
+  //   //                     className={`${
+  //   //                       value.month === months[currentMonth] &&
+  //   //                       value.date === currentDate &&
+  //   //                       value.year === currentYear
+  //   //                         ? "text-success"
+  //   //                         : ""
+  //   //                     }`}
+  //   //                   >
+  //   //                     <h2
+  //   //                       className={`${
+  //   //                         value.month !==
+  //   //                         months[+moment(selectDate).format("MM") - 1]
+  //   //                           ? "text-secondary"
+  //   //                           : ""
+  //   //                       } mt-3`}
+  //   //                     >
+  //   //                       {value.date}
+  //   //                     </h2>
+  //   //                   </span>
+  //   //                 </>
+  //   //               )}
+  //   //             </div>
+  //   //           </>
+  //   //         );
+  //   //       })}
+  //   //         </div>
+  //   //       );
+  //   //     })}
+  //   //   </div>
+  //   // </div>
+  //   // </>
+
+  //   <>
+  //     {/* <div className="container-fluid">
+  //       <div className="header d-flex justify-content-between align-items-center p-3">
+  //         <div>
+  //           <span className="fw-bold mx-2" style={{ fontSize: "50px" }}>
+  //             {months[+moment(selectDate).format("MM") - 1]}
+  //           </span>{" "}
+  //           <span className="h1 fw-normal" style={{ fontSize: "50px" }}>
+  //             {moment(selectDate).format("YYYY")}
+  //           </span>
+  //         </div>
+  //         <div>
+  //           <span>
+  //             <button onClick={getPrevMonth} className="btn  border border-2">
+  //               <GrPrevious />
+  //             </button>
+  //             <button className="btn border border-2 mx-2" onClick={getToday}>
+  //               Today
+  //             </button>
+  //             <button onClick={getNextMonth} className="btn border border-2">
+  //               <GrNext />
+  //             </button>
+  //           </span>
+  //         </div>
+  //       </div>
+  //       <div className="h-100 w-100  text-end">
+  //         <div className="row">
+  //           {days.map((val) => {
+  //             return (
+  //               <>
+  //                 <div className="col" key={val}>
+  //                   <h3 className="fw-normal">{val}</h3>
+  //                 </div>
+  //               </>
+  //             );
+  //           })}
+  //         </div>
+  //         {/* <div className="row">
+  //             {calanderArray.map((val, i) => {
+  //           Object.values(val).map((v)=>{
+  //             console.log(v.date, v.month);
+
+  //           })
+  //           return (
+  //             <>
+  //               <div className="col" key={val}>
+  //                   <h3 className="fw-normal">{val.date}</h3>
+  //                 </div>
+  //             </>
+  //           )
+  //             })}
+  //           </div> */}
+
+  //         {calanderArray.map((val, i) => {
+  //           console.log(val);
+  //           return (
+  //             <div className="row" style={{ height: "135px" }}>
+  //               {Object.values(val).map((value, i) => {
+
+  //                     <>
+  //                       <div
+  //                         className="col border"
+  //                         style={{
+  //                           background: `${i === 0 ? "#f0f0f2" : ""}`,
+  //                         }}
+  //                       >
+  //                         {value.date === 1 ? (
+  //                           <>
+  //                             <span
+  //                               className={`${
+  //                                 value.month === months[currentMonth] &&
+  //                                 value.date === currentDate &&
+  //                                 value.year === currentYear
+  //                                   ? "text-success"
+  //                                   : "bg-danger"
+  //                               }`}
+  //                             >
+  //                               <h2
+  //                                 className={`${
+  //                                   value.month !==
+  //                                   months[+moment(selectDate).format("MM") - 1]
+  //                                     ? "text-secondary"
+  //                                     : ""
+  //                                 } mt-3`}
+  //                               >
+  //                                 {value.date} {value.month.slice(0, 3)}
+  //                               </h2>
+  //                             </span>
+  //                           </>
+  //                         ) : (
+  //                           <>
+  //                             <span
+  //                               className={`${
+  //                                 value.month === months[currentMonth] &&
+  //                                 value.date === currentDate &&
+  //                                 value.year === currentYear
+  //                                   ? "text-success"
+  //                                   : ""
+  //                               }`}
+  //                             >
+  //                               <h2
+  //                                 className={`${
+  //                                   value.month !==
+  //                                   months[+moment(selectDate).format("MM") - 1]
+  //                                     ? "text-secondary"
+  //                                     : ""
+  //                                 } mt-3`}
+  //                               >
+  //                                 {value.date}
+  //                               </h2>
+  //                             </span>
+  //                           </>
+  //                         )}
+  //                       </div>
+  //                     </>
+  //                   ) : (
+  //                     <></>
+  //                   );
+  //                 }
+  //               })}
+  //             </div>
+  //           );
+  //         })}
+  //       </div>
+  //     </div>
+  //   </> */}
+  // );
+
   return (
     <>
       <div className="container-fluid">
@@ -64,10 +303,11 @@ export default function Calander() {
           </div>
 
           {Object.values(calanderRow).map((val) => {
+            console.log("ehfvwhebfiwefwe", val);
             return (
-              <div className="row" style={{ height: "135px" }}>
-                {val.map((value, i) => {
-                  console.log("Little Bunty", +moment(selectDate).format("MM"));
+              <div className="row" style={{ height: "155px" }}>
+                {Object.values(val).map((value, i) => {
+                  console.log("", +moment(selectDate).format("MM"));
 
                   return (
                     <>
--- src/Component/Moment.js
+++ src/Component/Moment.js
@@ -18,131 +18,88 @@ const Months = [
 
 const GetCalander = (days = Days, months = Months) => {
   let today = moment();
-  // console.log("Today", today);
-  // const daysInWeek = [0, 1, 2, 3, 4, 5, 6];
 
   let currentDate = +moment().format("DD");
-  // console.log("Current Date", currentDate);
 
   let currentMonth = +moment().format("MM") - 1;
-  // console.log("Current Month", currentMonth);
 
   let currentYear = +moment().format("YYYY");
-  // console.log("Current Year", currentYear);
-
-  // let currentDay = moment().format("dddd");
 
   const [selectDate, setSelectDate] = useState(today);
-  // console.log("selectDate", selectDate);
-
-  // let monthLastDate = moment(selectDate).endOf("month");
-  // console.log("monthlastdate", monthLastDate);
 
   let daysInMonth = moment(selectDate).daysInMonth();
-  // console.log("DaysInMonth", daysInMonth);
 
   let prevMonthLastDate = moment(selectDate)
     .subtract(1, "month")
     .endOf("month")
     .format();
-  // console.log("prevMonthLastDate", prevMonthLastDate.toString());
-
-  // let firstDayInMonth = moment(selectDate).startOf("month").format("ddd");
-  // console.log("firstDayInMonth", firstDayInMonth);
-  // console.log("firstDayInMonth", days.indexOf(firstDayInMonth));
 
   let currentMonthStartingPoint = +moment(selectDate)
     .startOf("month")
     .format("d");
-  // console.log("currentMonthStartingPoint", currentMonthStartingPoint);
 
   let prevMonthStartingPoint =
     moment(prevMonthLastDate).format("DD") - currentMonthStartingPoint + 1;
-  // console.log("prevMonthStartingPoint", prevMonthStartingPoint);
   let currentMonthCounter = 1;
   let nextMonthCounter = 1;
-
-  const row = 6;
-  const col = 7;
-
   const calanderRow = {};
 
-  for (let i = 1; i <= row; i++) {
-    for (let j = 1; j <= col; j++) {
-      if (!calanderRow[i]) {
-        calanderRow[i] = [];
-        // console.log("calanderRow", calanderRow);
-      }
-      if (i === 1) {
-        if (j <= currentMonthStartingPoint) {
-          calanderRow[i] = [
-            ...calanderRow[i],
-            {
-              month: `${months[+moment(selectDate).format("MM") - 2]}`,
-              date: prevMonthStartingPoint,
-              year: +moment(selectDate).format("YYYY"),
-            },
-          ];
-          prevMonthStartingPoint++;
-          // console.log("calanderRow", calanderRow);
-        } else {
-          calanderRow[i] = [
-            ...calanderRow[i],
-            {
-              month: `${months[+moment(selectDate).format("MM") - 1]}`,
-              date: currentMonthCounter,
-              year:
-                moment(selectDate).format("MM") - 1 === 0
-                  ? +moment(selectDate).format("YYYY") - 1
-                  : +moment(selectDate).format("YYYY"),
-            },
-          ];
-          currentMonthCounter++;
-        }
-      } else if (i > 1 && currentMonthCounter <= daysInMonth) {
-        calanderRow[i] = [
-          ...calanderRow[i],
-          {
-            month: `${months[+moment(selectDate).format("MM") - 1]}`,
-            date: currentMonthCounter,
-            year: +moment(selectDate).format("YYYY"),
-          },
-        ];
-        currentMonthCounter++;
-      } else {
-        // console.log(
-        //   "months[+moment(selectDate).format",
-        //   moment(selectDate).format("MM") == 12
-        // );
-        calanderRow[i] = [
-          ...calanderRow[i],
-          {
-            month: `${
-              moment(selectDate).format("MM") == 12
-                ? months[+moment(selectDate).format("MM") - 12]
-                : months[+moment(selectDate).format("MM")]
-            }`,
+  let columnRow = {};
+  let c = 0;
+  let b = 1;
 
-            date: nextMonthCounter,
-            year:
-              +moment(selectDate).format("MM") == 12
-                ? moment(selectDate).format("YYYY") + 1
-                : moment(selectDate).format("YYYY"),
-          },
-        ];
+  for (let i = 1; i <= 42; i++) {
+    if (c == 7) {
+      calanderRow[b] = { ...columnRow};
+      b++;
+      columnRow = {};
+      c = 0;
+    } else {
+      if (i <= currentMonthStartingPoint) {
+        columnRow[c] = {
+          month: `${months[+moment(selectDate).format("MM") - 2]}`,
+          date: prevMonthStartingPoint,
+          year: +moment(selectDate).format("YYYY"),
+          class: "Prev",
+        };
+        prevMonthStartingPoint++;
+        c++;
+      } else if (currentMonthCounter <= daysInMonth) {
+        columnRow[c] = {
+          month: `${months[+moment(selectDate).format("MM") - 1]}`,
+          date: currentMonthCounter,
+          year: +moment(selectDate).format("YYYY"),
+          class: "current",
+        };
+        currentMonthCounter++;
+        c++;
+      } else {
+        columnRow[c] = {
+          month: `${
+            moment(selectDate).format("MM") == 12
+              ? months[+moment(selectDate).format("MM") - 12]
+              : months[+moment(selectDate).format("MM")]
+          }`,
+
+          date: nextMonthCounter,
+          year:
+            +moment(selectDate).format("MM") == 12
+              ? moment(selectDate).format("YYYY") + 1
+              : moment(selectDate).format("YYYY"),
+          class: "Next",
+        };
         nextMonthCounter++;
+        c++;
       }
     }
   }
 
   const getPrevMonth = () => {
     setSelectDate(moment(selectDate).subtract(1, "month"));
-    // console.log("This is previous date", selectDate);
   };
 
   const getNextMonth = () => {
     setSelectDate(moment(selectDate).add(1, "month"));
-    // console.log("this si next date", selectDate);
   };
 
   const getToday = () => {
